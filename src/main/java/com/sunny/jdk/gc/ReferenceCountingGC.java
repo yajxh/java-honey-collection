80ae4c0a60af6a278974ed733d4d593f5711a620
e <yu.amgen@gmail.com>
2018-09-10 09:44:12 +0800

    OOM 和 GC

---
+++ src/main/java/com/sunny/jdk/gc/ReferenceCountingGC.java
@@ -0,0 +1,39 @@
+package com.sunny.jdk.gc;
+
+/**
+ * <Description> 引用计数算法的缺陷<br>
+ *
+ * @author Sunny<br>
+ * @version 1.0<br>
+ * @taskId: <br>
+ * @createDate 2018/09/06 14:01 <br>
+ * @see com.sunny.jdk.gc <br>
+ */
+public class ReferenceCountingGC {
+    public Object instance = null;
+
+    public static final int _1MB = 1024 * 1024;
+
+    /**
+     * 占点内存，以便GC日志观看
+     */
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void main(String[] args) {
+        testGC();
+    }
+
+    public static void testGC() {
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA = null;
+        objB = null;
+
+        //这里发生GC， objA 和 objB能否被回收？
+        System.gc();
+    }
+
+}
---
+++ src/main/java/com/sunny/jdk/oom/JavaVMStackOOM.java
@@ -0,0 +1,37 @@
+package com.sunny.jdk.oom;
+
+/**
+ * <Description> 创建线程导致内存溢出异常
+ * VM Args: -Xss2M<br>
+ * 注意：如果读者要尝试运行上面这段代码，记得先保存当前的工作。由于windows平台的虚拟机中，Java的线程是映射到操作系统的
+ *      内核线程上的，因此上述代码执行时有较大的风险，可能导致操作系统假死。
+ *
+ * @author Sunny<br>
+ * @version 1.0<br>
+ * @taskId: <br>
+ * @createDate 2018/09/06 9:50 <br>
+ * @see com.sunny.jdk.oom <br>
+ */
+public class JavaVMStackOOM {
+    private void dontStop() {
+        while (true) {
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
