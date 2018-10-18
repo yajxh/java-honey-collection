ac349f2d60ed53707ad51b480756cbf4ebe612c7
e <yu.amgen@gmail.com>
2018-10-18 15:29:42 +0800

    Netty Two way communication

---
+++ src/main/java/com/sunny/communication/netty/twoway/client/NettyClient.java
@@ -0,0 +1,64 @@
+package com.sunny.communication.netty.twoway.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <Description> <br>
+ *
+ * @author Sunny<br>
+ * @version 1.0<br>
+ * @taskId: <br>
+ * @createDate 2018/10/18 13:54 <br>
+ * @see com.sunny.communication.netty.twoway.client <br>
+ */
+public class NettyClient {
+    private static final int MAX_RETRY = 5;
+    public static final String HOST = "127.0.0.1";
+    public static final int PORT = 8000;
+
+    public static void main(String[] args) {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                .group(workerGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new FirstClientHandler());
+                    }
+                });
+        
+        connect(bootstrap, HOST, PORT, MAX_RETRY);
+    }
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接端口[" + port + "]成功！");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接端口[" + port + "]失败，第" + order + "次重连......");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1),
+                        delay, TimeUnit.SECONDS);
+            }
+        });
+    }
+}
