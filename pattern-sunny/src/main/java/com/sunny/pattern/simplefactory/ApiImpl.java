504bd18ac4d097c3e65fe720d884f37ffd2b759f
yajxh <yu.anjin@gmail.com>
2021-01-17 14:29:41 +0800

    简单工厂模式

--- pattern-sunny/src/main/java/com/sunny/pattern/simplefactory/ApiImpl.java
+++ pattern-sunny/src/main/java/com/sunny/pattern/simplefactory/ApiImpl.java
@@ -1,5 +1,11 @@
 package com.sunny.pattern.simplefactory;
 
+/**
+ * @description: TODO
+ * @author Sunny
+ * @date 2021/1/17 2:28 下午
+ * @version 1.0
+ */
 public class ApiImpl implements Api{
     @Override
     public void doApi() {
